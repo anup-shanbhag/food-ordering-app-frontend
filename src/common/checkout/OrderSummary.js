f558b93674823bc3f63455e3464704a2d1dcaa39
anup-shanbhag <anupshanbhag.sd11@iiitb.net>
2020-08-22 00:18:05 +0200

    Added OrderItem component

--- src/common/checkout/OrderSummary.js
+++ src/common/checkout/OrderSummary.js
@@ -1,52 +1,51 @@
 import React from 'react';
 import {Card, CardContent, Button, Typography, FormControl, CardActions, Divider, Box} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+import OrderItem from "./OrderItem";
+import "font-awesome/css/font-awesome.css"
 
-const useStyles = makeStyles({
-    veg: {
-        color: 'green'
-    },
-    nonVeg: {
-        color: 'red'
-    }
-});
-export default function OrderSummary(){
-    const classes = useStyles();
+export default function OrderSummary(props){
+
     const onClickPlaceOrder = () => {};
 
     return(
         <Card className="login-card" >
             <CardContent>
                 <Box display="flex" flexDirection="column">
-                    <FormControl margin="dense" size="small" variant="standard">
+                    <FormControl fullWidth margin="normal" size="small" variant="standard">
                         <Typography className="summary-title" variant="h6" color="textPrimary">
                             Summary
                         </Typography>
                     </FormControl>
-                    <FormControl margin="dense" size="small" variant="standard">
-                        <Typography className="restaurant-name" variant="body1" color="textPrimary">
-                            Loud Silence
+                    <FormControl fullWidth margin="dense" size="small" variant="standard">
+                        <Typography className="restaurant-name" variant="body1" color="textPrimary" component="p">
+                            {props.order.restaurant_name}
                         </Typography>
                     </FormControl>
-                    <FormControl margin="dense" size="small" variant="standard">
-
+                    <FormControl fullWidth margin="dense" size="small" variant="standard">
+                        {
+                            (props.order.item_quantities.length > 0) &&
+                                props.order.item_quantities.map(item => (
+                                    <OrderItem key={item.item.id}
+                                               nonVeg={item.item.type === "NON_VEG"}
+                                               name={item.item.item_name} quantity={item.quantity}
+                                               price={item.price}/>
+                                ))
+                        }
+                        <Typography variant="h3" gutterBottom/>
                     </FormControl>
-                    <Divider variant="middle" />
-                    <FormControl margin="dense" size="small" variant="standard">
-                        <Box display="flex" flexDirection="row">
-                                <Typography className="net-amount" variant="body1" color="textPrimary">
+                    <Divider variant="fullWidth" />
+                    <FormControl fullWidth margin="normal" size="small" variant="standard">
+                        <Box display="flex" flexDirection="row" justifyContent="space-between">
+                                <Typography className="net-amount" variant="body2" color="textPrimary">
                                     Net Amount
                                 </Typography>
-                            <Box display="flex" flexDirection="row" ml="auto" mr="1%">
-                                <Typography>R </Typography>
-                                <Typography>122.00</Typography>
-                            </Box>
+                                <Typography variant="body2"><i className="fa fa-inr"/> {Number(props.order.bill).toFixed(2)}</Typography>
                         </Box>
                     </FormControl>
                 </Box>
             </CardContent>
             <CardActions>
-                <FormControl margin="dense" size="small" variant="standard">
+                <FormControl fullWidth margin="normal" size="small" variant="standard">
                     <Button variant="contained" color="primary" id="btn-place" onClick={onClickPlaceOrder}>PLACE ORDER</Button>
                 </FormControl>
             </CardActions>
