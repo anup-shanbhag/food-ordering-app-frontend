bd299812388e0ab994049d1ac9b9bde78faf5550
alok.saboji@gmail.com”git config --global user.email alok.saboji@gmail.comgit initgit config --global user.name alok.saboji@gmail.com” <alok.saboji@gmail.com>
2020-08-23 20:46:07 +0530

    Making header responsive Showing search based on incoming prop 'showSearch'

---
+++ src/screens/profile/profile.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import Header from "../../common/header/Header";
+
+class Profile extends Component{
+    render(){
+        return(
+            <div>
+                <Header/>
+                <span>Profile</span>
+            </div>
+        )};
+}
